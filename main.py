c23ceb18fe6cd414fc4c701a66c315465f738547
situsina <67769401+situsina@users.noreply.github.com>
2023-05-23 08:34:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,29 +29,3 @@ try:
     content = result['msg']
 
 
-# 机场的地址
-url1 = os.environ.get('URL1')
-# 配置用户名（一般是邮箱）
-email1 = os.environ.get('EMAIL1')
-# 配置用户名对应的密码 和上面的email对应上
-passwd1 = os.environ.get('PASSWD1')
-
-login_url = '{}/auth/login'.format(url1)
-check_url = '{}/user/checkin'.format(url1)
-
-header = {
-        'origin': url1,
-        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
-}
-data = {
-        'email': email1,
-        'passwd': passwd1
-}
-try:
-    print('进行登录...')
-    response = json.loads(session.post(url=login_url,headers=header,data=data).text)
-    print(response['msg'])
-    # 进行签到
-    result = json.loads(session.post(url=check_url,headers=header).text)
-    print(result['msg'])
-    content = result['msg']
